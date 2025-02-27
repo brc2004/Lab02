4be3c3a5c1b998053d4bc2310c2a28098a37604d
saltybagel <briansegurachagolla@csus.edu>
2025-02-27 12:54:44 -0800

    Update it with information

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		// Add a new line with your information
 		System.out.println("302823131 - This line was added by Benjamin Chisum.");
+		System.out.println("302443596 - This line was added by Brian Segura Chagolla");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 	}
 }
