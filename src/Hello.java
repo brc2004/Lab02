8f5d35d47b46c68b5955ada8be57f020456b52ca
brchisum <brchisum@csus.edu>
2025-02-26 16:26:29 -0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		// Add a new line with your information
+		System.out.println("302823131 - This line was added by Benjamin Chisum.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+	}
+}
