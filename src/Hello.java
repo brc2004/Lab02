0840e6729df7119a0224edc8291a519d34d1386d
ImJeezus <ImJeezus@10.0.0.157>
2025-02-28 21:55:21 -0800

    Updated info

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("302823131 - This line was added by Benjamin Chisum.");
 		System.out.println("302443596 - This line was added by Brian Segura Chagolla");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("304278507 - This line was added by Lorencio Velez.");
 	}
 }
